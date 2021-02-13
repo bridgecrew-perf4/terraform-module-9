c4b78508782141831a1f7dfd2ab9174100923f23
algazali <egz7li@gmail.com>
2021-02-13 14:56:34 +0200

    updates

--- setup/main.tf
+++ setup/main.tf
@@ -5,9 +5,17 @@ provider "aws" {
 
 module "add-tags" {
    source   =  "../module/add-tags"
+}
+
+resource "aws_subnet" "subnet-in" {
+   id = data.aws_subnet.subnet_id
    tags = var.mytags
 }
 
 output "mod-info" {
-   value = module.add-tags.vpc-selected
+   value = module.add-tags
+}
+
+output "subnet-info" {
+   value = resource.aws_subnet.subnet-in
 }
