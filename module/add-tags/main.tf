d60f73ed11064a5a197ce794548b6d8b40b5a9d9
algazali <egz7li@gmail.com>
2021-02-13 10:31:17 +0200

    updates

--- module/add-tags/main.tf
+++ module/add-tags/main.tf
@@ -5,10 +5,11 @@ data "aws_vpc" "vpc-selected" {
       values = ["Main"]
    }
 }
-
 resource "aws_vpc" "vpc-selected" {
    tags = var.my-tags
 }
+
+
 data "aws_subnet" "subnet-pub"{
    cidr_block = var.cidr_block
 }
